66059f593e291eec31167d059ccf7fb4e3781af8
Mohamed Elhossiny <140903347+Melhossiny65@users.noreply.github.com>
2023-10-29 18:45:53 +0200

    MultiNode.sh

--- MultiNode.sh
+++ MultiNode.sh
@@ -8,7 +8,7 @@ do
 servers=(root@10.100.31.71 root@10.100.21.240)
   for x in "${servers[@]}"
      do
-         ssh $x md5sum /etc/pve/qemu-server/$i.conf | awk -F '/' '{print $1,$5}'
+         ssh $x md5sum /mnt/pve/templates/images/$i/base-$i-disk-0.qcow2
          ssh $x cat /etc/pve/qemu-server/$i.conf | grep name:
 done
 done
